04ccb326ad05d0f79d40896b1efcae6f95bf1e6b
duppahMeamnim <DuppahMeamnim@gmail.com>
2024-08-05 22:06:41 +0300

    ad
    ad

--- Assets/Player/GasManager.cs
+++ Assets/Player/GasManager.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GasManager : MonoBehaviour
 {
     [SerializeField] float maxGas;
     [SerializeField] float currentGas;
     [SerializeField] float gasRemoveRate;
+    [SerializeField] Slider gasSlider;
     public float CurrentGas { get => currentGas; set => currentGas = value; }
 
-    // Update is called once per frame
     void Update()
     {
         RemoveGas();
@@ -23,7 +24,7 @@ public class GasManager : MonoBehaviour
     private void RemoveGas()
     {
         currentGas = Mathf.Clamp(currentGas - (gasRemoveRate * Time.deltaTime), 0, maxGas);
-        print(currentGas);
+        gasSlider.value = currentGas / maxGas;
 
         if (currentGas <= 0)
         {

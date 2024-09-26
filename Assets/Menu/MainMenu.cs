5f96153f0a4c422d57c167ab42ddbd3599d4a1cb
duppahMeamnim <DuppahMeamnim@gmail.com>
2024-09-26 20:32:36 +0300

    asdasd
    asdasdasd

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -9,51 +9,15 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] Button playButton;
-    [SerializeField] TMP_InputField seedInput;
-
-    int seed = 0;
 
     private void Awake()
     {
         playButton.onClick.AddListener(OnPlayButtonClicked);
-        seedInput.onValueChanged.AddListener(OnSeedInputChanged);
-
-        seedInput.interactable = true;
-        seedInput.characterValidation = TMP_InputField.CharacterValidation.Integer;
     }
 
     private void OnPlayButtonClicked()
     {
-        if (seedInput.text == "")
-        {
-            SeedManager.Instance.isSeedOn = false;
-        }
-        else
-        {
-            SeedManager.Instance.isSeedOn = true;
-            SeedManager.Instance.seed = int.Parse(seedInput.text);
-        }
-
         //Yes, unity sucks, and this is the way to do it.
         SceneManager.LoadScene("EndlessScene");
-    }
-
-    private void OnSeedInputChanged(string newSeed)
-    {
-        if (newSeed == "" || newSeed == "-" || newSeed == "0")
-        {
-            seedInput.text = "";
-            return;
-        }
-
-        int newSeedInt = int.Parse(newSeed);
-        if (newSeedInt >= 1 && newSeedInt <= 133420)
-        {
-            seed = newSeedInt;
-        }
-        else
-        {
-            seedInput.text = seed.ToString();
-        }
     }
 }

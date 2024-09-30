c8f2e88299477a905970e823e3b569c309a97f09
duppahMeamnim <DuppahMeamnim@gmail.com>
2024-10-01 01:07:27 +0300

    NO BAG
    NO BAG

--- Assets/Battery/Gas.cs
+++ Assets/Battery/Gas.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gas : MonoBehaviour
@@ -24,12 +26,27 @@ public class Gas : MonoBehaviour
         Vector3 directionToGas = transform.position.normalized;
         float angleInRad = Mathf.Atan2(directionToGas.y, directionToGas.x);
 
-        float arcAngle = Mathf.PI * .9f;
-        float generationAngle = Random.Range(angleInRad - arcAngle / 2, arcAngle + angleInRad - arcAngle / 2);
+        float arcAngle = Mathf.PI * .6f;
+
+        Collider2D[] colliders;
+        Vector3 randomPointOnArc;
+        do
+        {
+            randomPointOnArc = GetRandomPointOnArcInDirection(angleInRad, arcAngle);
+            colliders = Physics2D.OverlapCircleAll(randomPointOnArc, .2f);
+
+        } while (colliders.Length != 0);
+
+        Instantiate(gameObject, randomPointOnArc, new Quaternion());
+    }
+
+    Vector3 GetRandomPointOnArcInDirection(float directionInRad, float arcAngle)
+    {
+        float generationAngle = Random.Range(directionInRad - arcAngle / 2, arcAngle + directionInRad - arcAngle / 2);
         float x = transform.position.x + generationRadius * Mathf.Cos(generationAngle);
         float y = transform.position.y + generationRadius * Mathf.Sin(generationAngle);
 
-        Instantiate(gameObject, new Vector3(x, y, 0), new Quaternion());
+        return new Vector3(x, y, 0);
     }
 
     //void OnDrawGizmos()

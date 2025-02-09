53cafab1fe3e68e3c3e852546d7035872577f1ba
DuppahMeamnim <DuppahMeamnim@gmail.com>
2025-02-09 19:31:55 +0200

    add logo

---
+++ Assets/GoToTwitter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToTwitter : MonoBehaviour
+{
+    public void OnClickLogo()
+    {
+        Application.OpenURL("https://x.com/MagnetiInc");
+    }
+}
